6a596a725d12fa6586c74dbb939d70928660e997
nandakishore15 <nandu15@live.com>
2019-04-18 14:20:08 -0400

    Fix main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,8 +2,8 @@ import akka.actor.{ActorSystem, Props}
 
 object Main extends App {
   // Assert to check if we have exactly two arguments
-  if (args.length != 3) {
-    println("Two arguments needed")
+  if (args.length != 4) {
+    println("Three arguments needed")
   }
   else {
     try {
